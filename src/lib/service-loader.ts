2d41731ac1551e4cefddd52a3f4630e8f89f2ceb
j4n-e4t <130256240+j4n-e4t@users.noreply.github.com>
2024-10-14 00:33:13 +0200

    load services alphabetically

---
+++ src/lib/service-loader.ts
@@ -0,0 +1,9 @@
+import type { Service } from "@/types/service";
+import services from "@/../data/services.json";
+
+function ServiceLoader(): Service[] {
+  services.sort((a, b) => a.name.localeCompare(b.name));
+  return services;
+}
+
+export default ServiceLoader;
